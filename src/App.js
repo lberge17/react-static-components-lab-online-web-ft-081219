4644485b4838f5c9266a213f5b0d27c8eaa86b92
Laura Berge <lauraeberge@gmail.com>
2020-07-12 18:20:42 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import CatComponent from './CatComponent';
+import CatComponent from 'CatComponent';
 import GraceHopperQuoteComponent from 'GraceHopperQuoteComponent';
 
 class App extends Component {
